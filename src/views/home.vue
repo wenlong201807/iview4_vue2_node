c8b6d72cddbc4008252322bdc6e0e347ed9a37b9
wenlongzhu <1573511441@qq.com>
2020-07-18 18:15:07 +0800

    添加面包屑功能雏形

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,83 +1,83 @@
 <template>
-  <div>
-    <div class="mainContainer">
-      <div class="menuCla">
-        <!-- <h1 class="test">首页home</h1> accordion -->
-        <Menu theme="primary" :open-names="['4-1']">
-          <Submenu name="1-0">
-            <template slot="title">
-              <Icon type="ios-people" />
-              实现功能的
-            </template>
-            <MenuItem to="/home/table" name="0-1">增删改查</MenuItem>
-            <MenuItem to="/home/calculate" name="0-2">计算器</MenuItem>
-            <MenuItem to="/home/definedData" name="0-3">自定义数据</MenuItem>
-            <MenuItem to="/home/Hightlight" name="0-4">代码高亮</MenuItem>
-            <MenuItem to="/home/TreeContexMenu" name="0-5">自定义右键</MenuItem>
-          </Submenu>
-          <Submenu name="1-1">
-            <template slot="title">
-              <Icon type="ios-paper" />
-              我的增删改查
-            </template>
-            <MenuItem to="/home/task_list" name="1-1">任务列表</MenuItem>
 
-            <MenuItem to="/home/statics" name="1-2">扫描统计</MenuItem>
-            <MenuItem to="/home/users" name="1-3">用户管理</MenuItem>
-          </Submenu>
-          <Submenu name="1-2">
-            <template slot="title">
-              <Icon type="ios-people" />
-              用户管理
-            </template>
-            <MenuItem name="2-1">新增用户</MenuItem>
-            <MenuItem name="2-2">活跃用户</MenuItem>
-          </Submenu>
+  <div class="mainContainer">
+    <div class="menuCla">
+      <!-- <h1 class="test">首页home</h1> accordion -->
+      <Menu theme="primary" :open-names="['4-1']">
+        <Submenu name="1-0">
+          <template slot="title">
+            <Icon type="ios-people" />
+            实现功能的
+          </template>
+          <MenuItem to="/home/table" name="0-1">增删改查</MenuItem>
+          <MenuItem to="/home/calculate" name="0-2">计算器</MenuItem>
+          <MenuItem to="/home/definedData" name="0-3">自定义数据</MenuItem>
+          <MenuItem to="/home/Hightlight" name="0-4">代码高亮</MenuItem>
+          <MenuItem to="/home/TreeContexMenu" name="0-5">自定义右键</MenuItem>
+        </Submenu>
+        <Submenu name="1-1">
+          <template slot="title">
+            <Icon type="ios-paper" />
+            我的增删改查
+          </template>
+          <MenuItem to="/home/task_list" name="1-1">任务列表</MenuItem>
 
-          <Submenu name="1-3">
-            <template slot="title">
-              <Icon type="ios-stats" />
-              统计分析
-            </template>
-            <MenuGroup title="使用">
-              <MenuItem name="3-1">新增和启动</MenuItem>
-              <MenuItem name="3-2">活跃分析</MenuItem>
-              <MenuItem name="3-3">时段分析</MenuItem>
-            </MenuGroup>
-            <MenuGroup title="留存">
-              <MenuItem name="3-4">用户留存</MenuItem>
-              <MenuItem name="3-5">流失用户</MenuItem>
-            </MenuGroup>
+          <MenuItem to="/home/statics" name="1-2">扫描统计</MenuItem>
+          <MenuItem to="/home/users" name="1-3">用户管理</MenuItem>
+        </Submenu>
+        <Submenu name="1-2">
+          <template slot="title">
+            <Icon type="ios-people" />
+            用户管理
+          </template>
+          <MenuItem name="2-1">新增用户</MenuItem>
+          <MenuItem name="2-2">活跃用户</MenuItem>
+        </Submenu>
 
-          </Submenu>
-          <Submenu name="1-4">
-            <template slot="title">
-              <Icon type="ios-people" />
-              echarts
-            </template>
-            <MenuItem to="/home/bar_ring" name="4-1">柱状图和环形</MenuItem>
-            <MenuItem to="/home/shanxing" name="4-2">扇形图</MenuItem>
-            <MenuItem to="/home/echart1" name="4-3">双指针示例</MenuItem>
-            <MenuItem to="/home/echart2" name="4-4">echart2指针图</MenuItem>
-            <MenuItem to="/home/echart3" name="4-5">echart3混合图1</MenuItem>
-            <MenuItem to="/home/echart4" name="4-6">echart4折线图2</MenuItem>
-            <MenuItem to="/home/echart5" name="4-7">echart5混合图2</MenuItem>
-            <MenuItem to="/home/VueLazyComponent" name="4-8">交叉监听</MenuItem>
-            <MenuItem to="/home/echartAll" name="4-9">echartAll混合图2</MenuItem>
-            <MenuItem to="/home/pro_echarts" name="4-10">组合展示图pro</MenuItem>
-          </Submenu>
-        </Menu>
+        <Submenu name="1-3">
+          <template slot="title">
+            <Icon type="ios-stats" />
+            统计分析
+          </template>
+          <MenuGroup title="使用">
+            <MenuItem name="3-1">新增和启动</MenuItem>
+            <MenuItem name="3-2">活跃分析</MenuItem>
+            <MenuItem name="3-3">时段分析</MenuItem>
+          </MenuGroup>
+          <MenuGroup title="留存">
+            <MenuItem name="3-4">用户留存</MenuItem>
+            <MenuItem name="3-5">流失用户</MenuItem>
+          </MenuGroup>
+
+        </Submenu>
+        <Submenu name="1-4">
+          <template slot="title">
+            <Icon type="ios-people" />
+            echarts
+          </template>
+          <MenuItem to="/home/bar_ring" name="4-1">柱状图和环形</MenuItem>
+          <MenuItem to="/home/shanxing" name="4-2">扇形图</MenuItem>
+          <MenuItem to="/home/echart1" name="4-3">双指针示例</MenuItem>
+          <MenuItem to="/home/echart2" name="4-4">echart2指针图</MenuItem>
+          <MenuItem to="/home/echart3" name="4-5">echart3混合图1</MenuItem>
+          <MenuItem to="/home/echart4" name="4-6">echart4折线图2</MenuItem>
+          <MenuItem to="/home/echart5" name="4-7">echart5混合图2</MenuItem>
+          <MenuItem to="/home/VueLazyComponent" name="4-8">交叉监听</MenuItem>
+          <MenuItem to="/home/echartAll" name="4-9">echartAll混合图2</MenuItem>
+          <MenuItem to="/home/pro_echarts" name="4-10">组合展示图pro</MenuItem>
+        </Submenu>
+      </Menu>
+    </div>
+    <div class="contentCla">
+      <div class="navCla">
+        <Button type="text" @click="loginout" class="loginOutCla">退出</Button>
       </div>
-      <div class="contentCla">
-        <div class="navCla">
-          <Button type="text" @click="loginout" class="loginOutCla">退出</Button>
-        </div>
-        <div class="contentPages">
-          <router-view />
-        </div>
+      <div class="contentPages">
+        <router-view />
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
---
+++ test.js
@@ -0,0 +1,164 @@
+
+let OriginArr = [
+  {
+    id: 1, parentId: -1, name: '第一层名字1', children: [
+      {
+        id: 21, name: '第二层名字21', children: [
+          { id: 131, name: '第三层名字31', children: [] },
+          { id: 132, name: '第三层名字32', children: [] },
+          { id: 133, name: '第三层名字32', children: [] },
+        ]
+      },
+      {
+        id: 212, name: '第二层名字212', children: []
+      },
+      {
+        id: 213, name: '第二层名字213', children: []
+      },
+    ]
+  },
+  {
+    id: 2, parentId: -1, name: '第一层名字2', children: [
+      {
+        id: 22, name: '第二层名字22', children: [
+          { id: 233, name: '第三层名字33', children: [] },
+          { id: 234, name: '第三层名字24', children: [] },
+          { id: 235, name: '第三层名字24', children: [] },
+        ]
+      },
+      {
+        id: 23, name: '第二层名字22', children: [
+          { id: 243, name: '第三层名字33', children: [] },
+          { id: 244, name: '第三层名字24', children: [] },
+          { id: 245, name: '第三层名字24', children: [] },
+        ]
+      },
+    ]
+  },
+  {
+    id: 3, parentId: -1, name: '第一层名字3', children: [
+      {
+        id: 232, name: '第二层名字232', children: [
+          { id: 333, name: '第三层名字333', children: [] },
+          { id: 334, name: '第三层名字324', children: [] },
+          { id: 335, name: '第三层名字324', children: [] },
+        ]
+      },
+      {
+        id: 233, name: '第二层名字233', children: [
+          { id: 243, name: '第三层名字243', children: [] },
+        ]
+      },
+      {
+        id: 234, name: '第二层名字234', children: []
+      },
+      {
+        id: 235, name: '第二层名字235', children: []
+      },
+    ]
+  },
+  // {
+  //   id: 3, parentId: -1, name: '第一层名字3', children: [
+  //     {
+  //       id: 3333, name: '第二层名字3333', children: [],
+  //       // id: 31, name: '第二层名字31', children: [], // 不出来？？？
+  //       id: 32, name: '第二层名字32', children: [],
+  //       id: 33, name: '第二层名字33', children: [],
+  //     },
+  //   ]
+  // },
+  {
+    id: 4, parentId: -1, name: '第一层名字4', children: []
+  },
+  {
+    id: 5, parentId: -1, name: '第一层名字5', children: []
+  },
+]
+
+let middleArr = [
+  { pid: -1, pname: '第一层名字100', id: 21, name: '第二层名字21', cid: 31, cname: '第三层名字31' },
+]
+
+let targetArr = [
+  { id: -1, name: '第一层名字100' },
+  { id: 21, name: '第二层名字21' },
+  { id: 31, name: '第三层名字31' },
+]
+
+function OriginArrTomiddleArr () {
+  let testArr = []
+  let obj = {}
+  // function nei (OriginArr) {
+  //   OriginArr.length && OriginArr.forEach((item, ind) => {
+  //     if (item.children.length) {
+  //       console.log('++++', ind)
+  //       nei(item.children)
+  //     } else {
+  //       console.log('====', ind)
+  //       obj = {
+  //         ...obj,
+  //         ['id' + ind]: item.id,
+  //         ['name' + ind]: item.name
+  //       }
+  //       testArr.push(obj)
+  //     }
+  //   })
+  // }
+  // nei (OriginArr)
+
+  OriginArr.forEach((itemOne, indexOne) => {
+    if (!itemOne.children.length) {
+
+      let obj = {
+        pid: itemOne.id, pname: itemOne.name,
+      }
+      testArr.push(obj)
+    }
+    itemOne.children.length && itemOne.children.forEach((itemTwo, indexTwo) => {
+      console.log('第二层：', indexTwo, itemTwo)
+      if (!itemTwo.children.length) {
+
+        let obj = {
+          pid: itemOne.id, pname: itemOne.name, id: itemTwo.id, name: itemTwo.name
+        }
+        testArr.push(obj)
+      }
+      itemTwo.children.length && itemTwo.children.forEach((itemThree, indexThree) => {
+        if (!itemThree.children.length) {
+
+          let obj = {
+            pid: itemOne.id, pname: itemOne.name, id: itemTwo.id, name: itemTwo.name, cid: itemThree.id, cname: itemThree.name
+          }
+          testArr.push(obj)
+        }
+      })
+    })
+  })
+  return testArr
+}
+
+console.log(OriginArrTomiddleArr())
+
+function middleArrTotargetArr (middleArr) {
+  let testArr2 = []
+  let itemObj = middleArr.length && middleArr[3]
+  // console.log('+++', Object.entries(itemObj))
+  let beforeMiaobaoList = Object.entries(itemObj)
+
+  for (let i = 0; i < beforeMiaobaoList.length; i++) {
+    console.log('++++++', i, i % 2)
+    if (i % 2) {
+      console.log('====', i, beforeMiaobaoList[i][1])
+      testArr2.push({ name: beforeMiaobaoList[i][1] })
+    } else {
+      console.log('----', 33)
+    }
+  }
+
+
+  return testArr2
+}
+
+// middleArrTotargetArr(OriginArrTomiddleArr())
+// console.log('====', middleArrTotargetArr(OriginArrTomiddleArr()))
+// console.log('===', middleArrTotargetArr(middleArr))
